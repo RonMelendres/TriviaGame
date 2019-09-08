dbe251ec685bd4f9c98377861d69a61a89037fb6
Ron Melendres <Ron.melendres25@gmail.com>
2019-09-07 17:50:37 -0700

    click timer needs work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,64 +1,149 @@
 $(document).ready(function () {
 
-// Intiate the game with an opening start button. Pressing this button will start the quiz
-// score vars:
-var right = 0;
-var wrong = 0;
-var noAnswer = 0;
-var time = 5;
-var intervalID;
 
-var questions = ("question-1", "question-2", "question-3");
-var showQuestions;
-var count = 0;
+    var right = 0;
+    var wrong = 0;
+    var noAnswer = 0;
+    var time = 5;
+    var intervalID;
+    var timeRun = false;
 
-var rightText = $("#correct-text").text("Correct: " + right);
-var wrongText = $("#incorrect-text").text("Inccorect: " + wrong);
-var noAnswerText = $("#unanswered-text").text("Unanswered: " + noAnswer);
-var timeText = $("#timer-text").text("Time Remaining: " + time);
+    var questions = $(".questions");
+    var showQuestions;
+    var count = 0;
 
-
-// function initializeGame(){
-//     right = 0;
-//     wrong = 0;
-//     noAnswer = 0;
-// }
-
-// $("#start").click(startQuiz);
-
-function showQuestion(){
-
-}
+    var rightText = $("#correct-text").text("Correct: " + right);
+    var wrongText = $("#incorrect-text").text("Inccorect: " + wrong);
+    var noAnswerText = $("#unanswered-text").text("Unanswered: " + noAnswer);
+    var timeText = $("#timer-text").text("Time Remaining: " + time);
 
 
-// Once the quiz starts, the first question is presented, and the timer starts counting down from 30.
+    // function initializeGame(){
+    //     right = 0;
+    //     wrong = 0;
+    //     noAnswer = 0;
+    // }
+
+
+    // Intiate the game with an opening start button. Pressing this button will start the quiz
+    // score vars:
+    $(".pages").hide();
+    $("#start").click(startQuiz)
     
-// timer function 
 
-timer();
-function timer() {
-    clearInterval(intervalID)
-    intervalID = setInterval(decrement, 1000 * 1);
-}
+    function startQuiz() {
+        $("#start").click(startTime);
+        $(".pages").show();
 
-function decrement() {
-    time--;
-    $("#timer-text").text("Time Remaining: " + time);
-    if (time === 0) {
-        stop();
+        // if ($("#start").click(startTime)) {
+        //     startTime();
+        // }
     }
-}
-
-function stop() {
-    clearInterval(intervalID);
-}
 
 
+    window.download = function () {
+        $("#start").on("click", startQuiz.start);
+    };
+
+    
+    var startTime = {
+
+        time: 5,
+
+        start: function () {
+        
+
+            if (!timeRun) {
+                timeRun = true;
+                intervalId = setInterval(startTime.count, 1000);
+                
+            
+            }
+
+            // if (timeRun === true){
+            //     time--;
+            // }
+
+        },
+
+        counter: function() {
+            $("#timer-text").text("Time Remaining: " + time);
+            time --;
+        },
+
+    // timer();
+    //     function timer() {
+
+    //         clearInterval(intervalID)
+    //         intervalID = setInterval(decrement, 1000 * 1);
+    //     }
+
+    //     function decrement() {
+            
+    //         if ($("#start").click(startQuiz)) {
+    //             time--;
+            
+    //         $("#timer-text").text("Time Remaining: " + time);
+    //         } if (time === 0) {
+    //             stop();
+    //         }
+    //     }
+
+    //     function stop() {
+    //         clearInterval(intervalID);
+    //     }
+
+    //     function startTime(){
+    //         // if ($("#start").click(startQuiz)) {
+    //         //     time--;
+    //         // }
+    //     }
 
 
-    // If the correct answer is guessed, timer stops, displays a "correct" message with the answer, and automatically moves to the next question after a few seconds
-    // If the incorrect answer is guessed or if the timer reachers zero, timer stops, displays "incorrect" message with the answer, and automatically moves to the next question after a few seconds
-    // This repeats until you finish the quiz.
+
+        // function startTime() {
+        //     showQuestions = setInterval(nextQuestion, 3000);
+
+        //   }
+
+        //   function revealQuestion(){
+        //     $(".pages").html(questions[count]);
+        // }
+
+        // function nextQuestion () {
+        //     count++;
+        //     $(".pages").html(questions);
+        //     if (count === questions.length) {
+        //         count = 0;
+        //     }
+        //     setTimeout(revealQuestion, 5000);
+        // }
+
+    
+
+        // decrement: function () {
+        //     time--;
+        //     $("#timer-text").text("Time Remaining: " + time);
+        //     if (time === 0) {
+        //         stop();
+        //     }
+        // },
+
+
+
+        // Once the quiz starts, the first question is presented, and the timer starts counting down from 30.
+
+        // timer function 
+
+
+
+
+
+
+
+        // If the correct answer is guessed, timer stops, displays a "correct" message with the answer, and automatically moves to the next question after a few seconds
+        // If the incorrect answer is guessed or if the timer reachers zero, timer stops, displays "incorrect" message with the answer, and automatically moves to the next question after a few seconds
+        // This repeats until you finish the quiz.
 
 
 
@@ -67,8 +152,8 @@ function stop() {
 
 
 
-// After the last question, regardless if answered right or wrong, the timer stops, results screen will pop up, and button to restart the quiz will pop up.
+        // After the last question, regardless if answered right or wrong, the timer stops, results screen will pop up, and button to restart the quiz will pop up.
 
 
 
-});
+    }})
